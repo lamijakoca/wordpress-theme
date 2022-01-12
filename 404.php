51edf2c898dcc350a52c8a5117b2c0be23ca747e
Lamija Koca <klamija99@gmail.com>
2022-01-12 22:36:20 +0100

    Not Found page

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header();?>
+    <div id="primary-error">
+        <h4 id="error-message">
+            <?php _e('It looks like nothing was found at this location. Maybe try a search?', 'LuckyTheme'); ?>
+            <?php get_search_form();?>
+        </h4>
+    </div>
