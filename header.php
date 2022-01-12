cda8b6487fccfd763bb1407c4eaf5418f3efde9d
Lamija Koca <klamija99@gmail.com>
2022-01-12 22:39:52 +0100

    najgluplje dodavanje css-a

--- header.php
+++ header.php
@@ -2,6 +2,36 @@
 <html lang="en">
 
 <head>
+    <style>
+    .topnav {
+    overflow: hidden;
+    /* background-color: #fff; */
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    }
+  
+    .topnav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    color: black;
+    text-decoration: none;
+    font-size: 17px;
+    }
+    .topnav a:hover {
+    background-color: #ddd;
+    color: green;
+    text-decoration: none;
+    }
+  
+    .topnav a.active {
+    color: green;
+    text-decoration: none;
+    }
+    </style>
+
     <title>STEM GAMES</title>
 
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
@@ -11,36 +41,10 @@
 </head>
 <body>
     <header id="masthead" class="site-header">
-        <nav id="primary-navigation" class="site-navigation">
-            <div class="container">
-
-                <div class="navbar-header">
-                   
-
-                </div>
-
-                <div class="collapse navbar-collapse" id="agency-navbar-collapse">
-
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <li class="active"><a href="index.html" data-toggle="dropdown">Home</a></li>
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages<i class="fa fa-caret-down hidden-xs" aria-hidden="true"></i></a>
-
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                              <li><a href="portfolio.html">Portfolio</a></li>
-                              <li><a href="blog.html">Blog</a></li>
-                            </ul>
-
-                        </li>
-                        <li><a href="portfolio.html">Portfolio</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                        <li><a href="ui-elements.html">UI Elements</a></li>
-
-                    </ul>
-
-                </div>
-
-            </div>   
-        </nav>
+        <div class="topnav">
+            <a class="active" href="http://localhost/wordpress/">Home</a>
+            <a href="404.php">News</a>
+            <a href="#site-footer">Contact</a>
+            <a href="http://localhost/wordpress/games/">Games</a>
+        </div>
     </header>
